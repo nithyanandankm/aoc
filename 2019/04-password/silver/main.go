d24114623640f4ce634b2dca9a2c2a1a4a3dcad0
Nithyanandan Manoranjan <n.manoranjan@catawiki.nl>
2019-12-06 13:35:38 +0100

    Add solution for day 4

---
+++ 2019/04-password/silver/main.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+func number_to_array(num int) []int {
+	res := make([]int, 0)
+
+	for num != 0 {
+		res = append(res, num%10)
+		num /= 10
+	}
+
+	for i, j := 0, len(res)-1; i < j; i, j = i+1, j-1 {
+		res[i], res[j] = res[j], res[i]
+	}
+
+	return res
+}
+
+func has_incremental_digits(passwd []int) bool {
+	for i := 0; i < len(passwd)-1; i++ {
+		var sequence = passwd[i+1]
+		if passwd[i] > sequence {
+			return false
+		}
+	}
+
+	return true
+}
+
+func has_equal_adjacent(passwd []int) bool {
+	for i := 0; i < len(passwd)-1; i++ {
+		var sequence = passwd[i+1]
+		if passwd[i] == sequence {
+			return true
+		}
+	}
+
+	return false
+}
+
+func validate_password_silver(passwd int) bool {
+	var passwd_array = number_to_array(passwd)
+
+	return has_incremental_digits(passwd_array) &&
+		has_equal_adjacent(passwd_array)
+}
+
+func main() {
+	var lower_limit, upper_limit = 136760, 595730
+	matches := 0
+
+	for i := lower_limit; i <= upper_limit; i++ {
+		var valid = validate_password_silver(i)
+
+		if valid {
+			matches += 1
+		}
+
+	}
+
+	fmt.Println("Combinations found: ", matches)
+}
